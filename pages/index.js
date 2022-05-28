eb81c382368d456033c2090b481b932538a22e9b
ZikaNen <106437048+ZikaNen@users.noreply.github.com>
2022-05-28 15:37:07 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,55 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+<!DOCTYPE html>
+<html>
+    <head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
 
-      <Footer />
-    </div>
-  )
-}
+        <meta name="viewport" content="widt=device-with, initial-scale1.0">
+        <title>hello</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="lolas">
+            <nav>
+                <audio autoplay loop>
+                    <source src="chill.mp3" type="audio/mp3">
+                </audio>
+
+
+
+
+                <video autoplay loop muted plays-inline class="back-video">
+
+                    <source src="DarkWinterPath.mp4" type="video/mp4">
+                </video>
+                
+                <ul>
+                    <li><a href="#"></a></li>
+                </ul>
+            </nav>
+            <div class="content">
+                
+                <div class="container">
+                    <div class="btn"><a href="https://steamcommunity.com/groups/cheatingistillegal">GROUP</a></div>  
+                    
+                  </div>
+                  <span title=0838#6568></span> 
+
+                    <h1></h1>
+                   
+                    <body>
+                        <div class="waviy">
+                                
+                         </div> 
+         
+
+
+                
+
+                
+                
+            </div>
+        </div>
+
+    </body>
+</html>
